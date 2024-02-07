51c0fd84ed9edea2b8b52c48ed97ad4399e1a5e9
fiv <1647715359@qq.com>
2024-02-07 15:17:23 +0800

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# PySt
-Python and Rust call each other
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    let mut a = 10;
+    let b = 20;
+    println!("The sum of {} and {} is {}", a, b, a + b);
+    a = 20;
+}
